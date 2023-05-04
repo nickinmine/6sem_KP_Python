f3bb6e59efc919c93bde128a4e0efe3e3c7117ac
Nick <nick.a.zubov@gmail.com>
2023-05-04 23:52:11 +0300

    database time zone fix

--- db_init/dump-python_coursework-202304160104.sql
+++ db_init/dump-python_coursework-202304160104.sql
@@ -8,6 +8,7 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+set time zone 'Europe/Moscow';
 
 CREATE FUNCTION public.thread_is_opened_checker() RETURNS trigger
     LANGUAGE plpgsql
