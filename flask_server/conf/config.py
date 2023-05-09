e83ebd77912b184cec7dcf4bff14784ba454808f
Nick <nick.a.zubov@gmail.com>
2023-05-09 18:57:04 +0300

    database connection fix 2

--- flask_server/conf/config.py
+++ flask_server/conf/config.py
@@ -5,5 +5,5 @@ db = SQLAlchemy()
 class Config(object):
     SECRET_KEY = '14b10598580fa862670c5ce365839c3b14b10598580fa862670c5ce365839c3b'
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:root@postgres/python_coursework'
-    SQLALCHEMY_DATABASE_URI = 'postgres://python_coursework_user:Zb6yd2D03kOvvy3S9IE3HeC50eqsPXiq@dpg-chd6dn2k728tp9f2audg-a/python_coursework'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://python_coursework_user:Zb6yd2D03kOvvy3S9IE3HeC50eqsPXiq@dpg-chd6dn2k728tp9f2audg-a/python_coursework'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
